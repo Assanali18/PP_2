9e40b06d5a43c0f29a4fa5b1477f36a866b7e4ca
Assanali18 <ustaevasanali@gmail.com>
2023-01-21 04:05:18 +0600

    first

---
+++ TSIS 1/bool.py
@@ -0,0 +1,25 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print()
+###########
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  print()
+############
+
+
+a = 0
+b =100
+c = "qqq00"
+d = ""
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))

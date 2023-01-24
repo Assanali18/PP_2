c9a4f3eea8a62bff34a048d0e64615ac23a9bb7b
Assanali18 <ustaevasanali@gmail.com>
2023-01-24 15:53:21 +0600

    first

---
+++ seminar(24.01)/2_8.py
@@ -0,0 +1,6 @@
+col_1 = int(input())
+li_1 = int(input())
+col_2 = int(input())
+li_2 = int(input())
+print( 'YES' if (col_1 + 1 == col_2 or col_1 - 1 == col_2 or col_1 == col_2) 
+		and (li_1 + 1 == li_2 or li_1 - 1 == li_2 or li_1 == li_2 )else 'NO')

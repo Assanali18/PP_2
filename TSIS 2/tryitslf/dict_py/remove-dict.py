58381b36c45ae6b011ffc68f172ec96570b1f410
Assanali18 <ustaevasanali@gmail.com>
2023-01-29 02:19:51 +0600

    first

---
+++ TSIS 2/tryitslf/dict_py/remove-dict.py
@@ -0,0 +1,27 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+# {'brand': 'Ford', 'year': 1964}
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+# {'brand': 'Ford', 'model': 'Mustang'}
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+#{'brand': 'Ford', 'year': 1964}
+
---
+++ TSIS 2/tryitslf/sets_py/acces_sets.py
@@ -0,0 +1,11 @@
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset) # True
+
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x) # apple cherry banana
+
+
+
+  

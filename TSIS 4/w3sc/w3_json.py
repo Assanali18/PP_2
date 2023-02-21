24667a69f325787f5554efbe780479d2ee778e8d
Assanali18 <ustaevasanali@gmail.com>
2023-02-21 13:54:40 +0600

    first

---
+++ TSIS 4/w3sc/w3_json.py
@@ -0,0 +1,157 @@
+import json
+
+# some JSON:
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"]) #30
+#########
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y) #{'name': 'John', 'age': 30, 'city': 'New York'}
+##########
+
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+# {"name": "John", "age": 30}
+# ["apple", "bananas"]
+# ["apple", "bananas"]
+# "hello"
+# 42
+# 31.76
+# true
+# false
+# null
+###########
+
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+#{"name": "John", "age": 30, "married": true, "divorced": false, "children": ["Ann","Billy"], "pets": null, "cars": [{"model": "BMW 230", "mpg": 27.5}, {"model": "Ford Edge", "mpg": 24.1}]}
+##########
+
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
+# {
+#     "name": "John",
+#     "age": 30,
+#     "married": true,
+#     "divorced": false,
+#     "children": [
+#         "Ann",
+#         "Billy"
+#     ],
+#     "pets": null,
+#     "cars": [
+#         {
+#             "model": "BMW 230",
+#             "mpg": 27.5
+#         },
+#         {
+#             "model": "Ford Edge",
+#             "mpg": 24.1
+#         }
+#     ]
+# }
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+# {
+#     "name" = "John".
+#     "age" = 30.
+#     "married" = true.
+#     "divorced" = false.
+#     "children" = [
+#         "Ann".
+#         "Billy"
+#     ].
+#     "pets" = null.
+#     "cars" = [
+#         {
+#             "model" = "BMW 230".
+#             "mpg" = 27.5
+#         }.
+#         {
+#             "model" = "Ford Edge".
+#             "mpg" = 24.1
+#         }
+#     ]
+# }
+
+print(json.dumps(x, indent=4, sort_keys=True))
+# {
+#     "age": 30,
+#     "cars": [
+#         {
+#             "model": "BMW 230",
+#             "mpg": 27.5
+#         },
+#         {
+#             "model": "Ford Edge",
+#             "mpg": 24.1
+#         }
+#     ],
+#     "children": [
+#         "Ann",
+#         "Billy"
+#     ],
+#     "divorced": false,
+#     "married": true,
+#     "name": "John",
+#     "pets": null
+# }
+
